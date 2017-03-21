77d6f44d10d04af6edfe9253b308fa9e1b4513be
dingbingshuang <1114328383@qq.com>
2017-03-21 16:58:03 +0800

    1

---
+++ js/style.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2017-03-20 10:58:35
+ * @version $Id$
+ */
+
+    	$(document).ready(function(){
+                 $(".content").height($(window).height());
+                  $(".content").width($(window).width());
+});
+    	$(window).resize(function(){
+     $(".content").height($(window).height());
+                  $(".content").width($(window).width());
+});
